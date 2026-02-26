8c5af24885e96be961398c8803b0e00508585ac0
232522student <232522@students.au.edu.pk>
2026-02-27 00:38:11 +0500

    Add loops program

---
+++ loops.py
@@ -0,0 +1,47 @@
+#For loop
+for i in range(1, 8):
+    print(i)
+
+for j in range(10,20):
+    print(j)
+
+for a in range(1,15,2):
+    print(a)
+
+for j in range(1,100,5):
+    print(j)
+
+for num in range(1, 11):
+    print("Table of", num)
+    for i in range(1, 11):
+        print(num, "x", i, "=", num * i)
+    print()
+
+for i in range(1, 6):
+    print("*" * i)
+
+for i in range(1, 8):
+    if i > 3:
+        print(i)
+
+#while Loop 
+x=4
+while x<=10:
+    print(x)
+    x=x+1
+
+a=1
+while a<=5:
+    print(a)
+    a=a+1
+
+y=2
+while y<=10:
+    print(y)
+    y=y+2
+
+b=5
+while b<=50:
+    print(b)
+    b=b+5
+
